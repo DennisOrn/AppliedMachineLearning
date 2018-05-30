b815128ab00f5230816bc96e9fb7fbd88b9074b0
Dennis Örnberg <dennis.ornberg@gmail.com>
2018-05-30 08:55:21 +0200

    Tried using a LabelEncoder

--- project/test.py
+++ project/test.py
@@ -4,20 +4,45 @@ import pandas as pd
 
 print('predict circuit based on year and round')
 
-data = pd.read_csv('data/races.csv')
-feature_cols = ['year', 'round']
+df = pd.read_csv('data/races.csv')
+feature_cols = ['year', 'date']
 
-# X = data.loc[:data.last_valid_index() - 1, feature_cols]
-X = data.loc[:, feature_cols]
-y = data.name
+le = LabelEncoder()
+le.fit(df.date.unique())
+df.date = le.transform(df.date)
+
+# X = df.loc[:df.last_valid_index() - 1, feature_cols]
+X = df.loc[:, feature_cols]
+y = df.name
 # print(X)
 # print(y)
+
+
+
+
+
+
+
+# le.fit(['one', 'two', 'two', 'three'])
+# print(le.classes_)
+# print(le.transform(['one', 'two', 'two', 'three']))
+# print(le.inverse_transform(le.transform(['one', 'two', 'two', 'three'])))
+
+print(X)
+
+
+inv = le.inverse_transform([1, 2, 3, 4, 5])
+print(inv)
+
+
+
+
 
 classifier = DecisionTreeClassifier()
 classifier.fit(X, y)
 
-d = {'year': [2019, 2019, 2019, 2019, 2019],
-     'round': [1, 2, 3, 4, 5]}
+d = {'year': [2018, 2018, 2018, 2018, 2018],
+     'date': [1, 2, 3, 4, 5]}
 df = pd.DataFrame(data=d)
 prediction = classifier.predict(df)
 print(prediction)
