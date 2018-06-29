ae3c59743209a9c5a5c0d27bd19f5861e104e9d2
Dennis Örnberg <dennis.ornberg@gmail.com>
2018-06-29 14:37:03 +0200

    Added a pretty worthless neural network

---
+++ project/test.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
+from xgboost import XGBClassifier
+
+# Load the results dataset into a dataframe.
+df = pd.read_csv('data/results.csv')
+
+# Extract the results from the 2017 season.
+df_2017 = df[(df.raceId >= 969) & (df.raceId <= 988)]
+
+# Train with the first 19 races, test with the last race.
+train = df_2017[:-20]
+test = df_2017[-20:]
+
+# Try to predict the position based on a few other features.
+y_train = train.positionOrder
+y_test = test.positionOrder
+
+columns = ['raceId', 'driverId', 'grid', 'rank']
+X_train = train[columns]
+X_test = test[columns]
+
+# Replace all NaN's with 0.
+X_train = X_train.fillna(0)
+y_train = y_train.fillna(0)
+X_test = X_test.fillna(0)
+y_test = y_test.fillna(0)
+
+# Create and train the classifiers.
+print('Training random forest classifier...')
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, y_train)
+
+print('Training xgboost classifier...')
+boost = XGBClassifier()
+boost.fit(X_train, y_train)
+
+print('Training neural network classifier...')
+neural_network = MLPClassifier(hidden_layer_sizes=(100, 100, 100))
+neural_network.fit(X_train, y_train)
+
+# Make predictions and print the accuracy.
+def predict(classifier):
+
+    print(type(classifier))
+
+    prediction = classifier.predict(X_test)
+    prediction = [round(value) for value in prediction]
+
+    print(f'prediction: {prediction}')
+    print(f'reality:    {y_test.tolist()}')
+
+    accuracy = accuracy_score(y_test, prediction)
+    print(f'accuracy:   {accuracy * 100}%\n')
+
+predict(random_forest)
+predict(boost)
+predict(neural_network)
